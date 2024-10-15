a2c696523522ca0d453b513df2b555c5f71ee7c6
Alistair-D <dickman.a1@stu.janesville.k12.wi.us>
2024-10-15 09:38:39 -0500

    10/15/2024

---
+++ LP4-2/LP4-2/Program.py
@@ -0,0 +1,22 @@
+﻿
+print int(input("Enter Package Weight In Kilograms: "))
+print int(input("Enter Package Length in Centimters: "))
+print int(input("Enter Package Width in Centimters: "))
+print int(input("Enter Package Height in Centimters: "))
+
+CubicMeter = Length * Width * Height
+
+if Weight <= 27 and CubicMeter <= 100000:
+    print("Package is Good")
+elif Weight > 27 and CubicMeter <= 100000:
+    print("Too Heavy")
+elif Weight <= 27 and CubicMeter > 100000:
+    print("Too Large")
+elif Weight > 27 and CubicMeter > 100000:
+    print("Too Large and Heavy")
+else:
+    print("Invalid Package Logistics")
+
+
+
+input()
