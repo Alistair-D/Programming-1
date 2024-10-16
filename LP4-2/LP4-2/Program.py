67d49cb796ce258e20d73d67c5e423c00be7c958
Alistair-D <dickman.a1@stu.janesville.k12.wi.us>
2024-10-16 09:33:49 -0500

    10/16/2024

--- LP4-2/LP4-2/Program.py
+++ LP4-2/LP4-2/Program.py
@@ -1,22 +1,21 @@
 ﻿
-print int(input("Enter Package Weight In Kilograms: "))
-print int(input("Enter Package Length in Centimters: "))
-print int(input("Enter Package Width in Centimters: "))
-print int(input("Enter Package Height in Centimters: "))
+Weight =  int(input("Enter Package Weight In Kilograms: "))
+Length =  int(input("Enter Package Length in Centimters: "))
+Width =  int(input("Enter Package Width in Centimters: "))
+Height = int(input("Enter Package Height in Centimters: "))
 
 CubicMeter = Length * Width * Height
 
 if Weight <= 27 and CubicMeter <= 100000:
-    print("Package is Good")
+    print ("Package is Good")
 elif Weight > 27 and CubicMeter <= 100000:
-    print("Too Heavy")
+    print ("Too Heavy")
 elif Weight <= 27 and CubicMeter > 100000:
-    print("Too Large")
+    print ("Too Large")
 elif Weight > 27 and CubicMeter > 100000:
-    print("Too Large and Heavy")
+    print ("Too Large and Heavy")
 else:
     print("Invalid Package Logistics")
-
 
 
 input()
---
+++ LP4-3/LP4-3/Program.py
@@ -0,0 +1,23 @@
+﻿EggCount = int(input("Enter #Amount of Eggs Purchased:"))
+
+price = 0
+Dozens = EggCount// 12
+remaining = EggCount % 12
+remainB = remaining * 0.1
+TotalDozen = Dozens + remainB
+print str(TotalDozen)
+
+if TotalDozen >= 0 and TotalDozen < 4 :
+    price = 0.50
+elif TotalDozen >= 4 and TotalDozen < 6 :
+    price = 0.45
+elif TotalDozen >=6 and TotalDozen < 11 :
+    price = 0.40
+elif TotalDozen <= 11:
+    price = 0.35
+
+totalprice = float(round(12*price,2)) * TotalDozen
+
+print "Your Egg Cost:" + str(round(totalprice,2))
+
+input()
