8d291a34847db69b504fa87689644810e29a89b4
Alistair-D <dickman.a1@stu.janesville.k12.wi.us>
2024-10-31 09:35:12 -0500

    10/31/24

--- Prog152b/Prog152b/MainForm.py
+++ Prog152b/Prog152b/MainForm.py
@@ -9,9 +9,108 @@ class MainForm(Form):
         self.InitializeComponent()
     
     def InitializeComponent(self):
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._label1 = System.Windows.Forms.Label()
+        self.SuspendLayout()
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(192, 255, 255)
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 12.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(-2, 401)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(217, 41)
+        self._button1.TabIndex = 0
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(192, 255, 255)
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 12.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(304, 401)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(217, 41)
+        self._button2.TabIndex = 1
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.FromArgb(192, 255, 255)
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 12.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(615, 401)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(217, 41)
+        self._button3.TabIndex = 2
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # listBox1
+        # 
+        self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 14, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 24
+        self._listBox1.Location = System.Drawing.Point(12, 96)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(787, 292)
+        self._listBox1.TabIndex = 3
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 16.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(258, 21)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(318, 32)
+        self._textBox1.TabIndex = 4
+        # 
+        # label1
+        # 
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 16.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(50, 21)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(181, 23)
+        self._label1.TabIndex = 5
+        self._label1.Text = "Input Test Value:"
         # 
         # MainForm
         # 
+        self.BackColor = System.Drawing.Color.FromArgb(255, 255, 192)
+        self.ClientSize = System.Drawing.Size(833, 442)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._listBox1)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
         self.Name = "MainForm"
         self.Text = "Prog152b"
+        self.Load += self.MainFormLoad
+        self.ResumeLayout(False)
+        self.PerformLayout()
 
+
+    def Button1Click(self, sender, e):
+        Value = 0
+        input = int(self._textBox1.Text)
+        for num in range(2,input+1,2):
+            Value += num
+            rest = str(num) + "\t\t" + str(Value)
+            self._listBox1.Items.Add(rest)
+
+
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def MainFormLoad(self, sender, e):
+        pass
